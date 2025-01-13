415511b65467690f5fee7d61d0fd066844b25545
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-01-13 15:03:29 +0200

    Draw enemies on the canvas

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const playerWidth = 60;
 const playerHeight = 30;
 let playerX = (canvas.width - playerWidth) / 2;
 const player = new Image();
+const enemy = new Image();
+const enemyWidth = 75;
+const enemyHeight = 20;
+const enemyPadding = 20;
+const enemyOffsetTop = 10;
+const enemyOffsetLeft = 20;
 let dx = 4;
 let dy = 4;
 let rightPressed = false;
 let leftPressed = false;
+
+const enemyRowCount = 3;
+const enemyColumnCount = 5;
+
+const enemies = [];
+for (let i = 0; i < enemyRowCount; i++) {
+  enemies[i] = [];
+  for (let j = 0; j < enemyColumnCount; j++) {
+    enemies[i][j] = { x: 0, y: 0, status: 1 };
+  }
+}
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
@@ -33,12 +50,26 @@ function keyUpHandler(e) {
 
 function init() {
   player.src = 'images/player.png';
+  enemy.src = 'images/green.png';
   window.requestAnimationFrame(draw);
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(player, playerX, canvas.height - playerHeight - 10);
+  for (let i = 0; i < enemyRowCount; i++) {
+    for (let j = 0; j < enemyColumnCount; j++) {
+      if (enemies[i][j].status === 1) {
+        const enemyX =
+          j * (enemyWidth + enemyPadding) + enemyOffsetLeft;
+        const enemyY =
+          i * (enemyHeight + enemyPadding) + enemyOffsetTop;
+        enemies[i][j].x = enemyX;
+        enemies[i][j].y = enemyY;
+        ctx.drawImage(enemy, enemyX, enemyY);
+      }
+    }
+  }
   if (rightPressed && playerX < canvas.width - playerWidth - 2) {
     playerX += dx;
   }
