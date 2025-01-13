284ab6f15114ca63b31a48cbd5fc11f955c58954
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-01-13 14:35:01 +0200

    Add horizontal player movement

--- index.js
+++ index.js
@@ -3,17 +3,49 @@ const ctx = canvas.getContext('2d');
 
 const playerWidth = 60;
 const playerHeight = 30;
+let playerX = (canvas.width - playerWidth) / 2;
+const player = new Image();
+let dx = 2;
+let dy = 2;
+let rightPressed = false;
+let leftPressed = false;
 
-function drawPlayer() {
-  const img = new Image();
-  img.addEventListener('load', () => {
-    ctx.drawImage(
-      img,
-      (canvas.width - playerWidth) / 2,
-      canvas.height - playerHeight - 10
-    );
-  });
-  img.src = 'images/player.png';
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.code === 'ArrowRight') {
+    rightPressed = true;
+  }
+  if (e.code === 'ArrowLeft') {
+    leftPressed = true;
+  }
 }
 
-drawPlayer();
+function keyUpHandler(e) {
+  if (e.code === 'ArrowRight') {
+    rightPressed = false;
+  }
+  if (e.code === 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
+
+function init() {
+  player.src = 'images/player.png';
+  window.requestAnimationFrame(draw);
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(player, playerX, canvas.height - playerHeight - 10);
+  if (rightPressed) {
+    playerX += dx;
+  }
+  if (leftPressed) {
+    playerX -= dx;
+  }
+  requestAnimationFrame(draw);
+}
+
+init();
