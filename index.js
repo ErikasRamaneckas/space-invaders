6a1a65a1b0978ae7d6808eb9a346d2025faa4b5c
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-01-13 14:45:06 +0200

    Add unit collision on player's ship

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const playerWidth = 60;
 const playerHeight = 30;
 let playerX = (canvas.width - playerWidth) / 2;
 const player = new Image();
-let dx = 2;
-let dy = 2;
+let dx = 4;
+let dy = 4;
 let rightPressed = false;
 let leftPressed = false;
 
@@ -39,10 +39,10 @@ function init() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(player, playerX, canvas.height - playerHeight - 10);
-  if (rightPressed) {
+  if (rightPressed && playerX < canvas.width - playerWidth - 2) {
     playerX += dx;
   }
-  if (leftPressed) {
+  if (leftPressed && playerX > 2) {
     playerX -= dx;
   }
   requestAnimationFrame(draw);
