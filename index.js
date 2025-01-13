bf79acb28268aeeb8036559c7fd6d18f8d55ebd1
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-01-13 09:49:26 +0200

    Draw player on the canvas

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
+
+const playerWidth = 60;
+const playerHeight = 30;
+
+function drawPlayer() {
+  const img = new Image();
+  img.addEventListener('load', () => {
+    ctx.drawImage(
+      img,
+      (canvas.width - playerWidth) / 2,
+      canvas.height - playerHeight - 10
+    );
+  });
+  img.src = 'images/player.png';
+}
+
+drawPlayer();
