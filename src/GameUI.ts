b690368fe78a3d59ce7f8410b6ff2a10e8aa6c5a
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-16 18:40:50 +0200

    Add classic arcade font style

--- src/GameUI.ts
+++ src/GameUI.ts
@@ -1,21 +1,25 @@
 export default class GameUI {
   score: number;
   lives: number;
+  fontStyle: string;
+  fontColor: string;
 
   constructor(score: number, lives: number) {
     this.score = score;
     this.lives = lives;
+    this.fontStyle = '48px ArcadeClassic, sans-serif';
+    this.fontColor = 'white';
   }
 
   drawScore(ctx: CanvasRenderingContext2D) {
-    ctx.font = '48px serif';
-    ctx.fillStyle = 'white';
+    ctx.font = this.fontStyle;
+    ctx.fillStyle = this.fontColor;
     ctx.fillText(this.score.toString(), 10, 40);
   }
 
   drawLives(ctx: CanvasRenderingContext2D) {
-    ctx.font = '48px serif';
-    ctx.fillStyle = 'white';
+    ctx.font = this.fontStyle;
+    ctx.fillStyle = this.fontColor;
     ctx.fillText(this.lives.toString(), 420, 40);
   }
 }
