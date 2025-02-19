195ba95977a104f6eaec197eff79e30cd3c49e0a
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-19 14:17:29 +0200

    Change red.png path

--- dist/Enemy.js
+++ dist/Enemy.js
@@ -11,8 +11,9 @@ export default class Enemy {
         this.y = y;
         this.speed = 0.3;
         this.enemyImg = new Image();
-        this.enemyImg.src =
-            'https://erikasramaneckas.github.io/space-invaders/red.png';
+        // this.enemyImg.src =
+        //   'https://erikasramaneckas.github.io/space-invaders/red.png';
+        this.enemyImg.src = 'red.png';
         this.width = ENEMY_WIDTH;
         this.height = ENEMY_HEIGHT;
     }
