63917d8b986f2493847bcf7ef630b5ccd86e735b
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-19 13:58:26 +0200

    Change images' filepaths

--- dist/Player.js
+++ dist/Player.js
@@ -23,7 +23,7 @@ export default class Player {
         this.x = initialX;
         this.y = CANVAS_HEIGHT - this.height - this.paddingY;
         this.image = new Image();
-        this.image.src = '../player.png';
+        this.image.src = '/space-invaders/player.png';
         this.speed = 4;
         this.bullets = [];
         this.lastShot = 0;
