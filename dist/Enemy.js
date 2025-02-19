e0f8ab9d127bc14c62a266eb3b19c7d90ca001bb
ErikasRamaneckas <erikasramaneckas@gmail.com>
2025-02-19 13:52:45 +0200

    Change images' filepaths

--- dist/Enemy.js
+++ dist/Enemy.js
@@ -11,7 +11,7 @@ export default class Enemy {
         this.y = y;
         this.speed = 0.3;
         this.enemyImg = new Image();
-        this.enemyImg.src = 'red.png';
+        this.enemyImg.src = '../red.png';
         this.width = ENEMY_WIDTH;
         this.height = ENEMY_HEIGHT;
     }
